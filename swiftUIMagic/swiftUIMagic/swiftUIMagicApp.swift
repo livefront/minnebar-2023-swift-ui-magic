a083734c530bbcfa16370edfe6bd5f75879a3d2c
Paul Himes <paul@livefront.com>
2023-03-21 13:23:50 -0500

    Initial project commit.

---
+++ swiftUIMagic/swiftUIMagic/swiftUIMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIMagicApp.swift
+//  swiftUIMagic
+//
+//  Created by Paul Himes on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
